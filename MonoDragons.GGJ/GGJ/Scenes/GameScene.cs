faba3204d9ac868bbb0f61d5e75d69d59b218616
Tim Reinagel <tmr2_handbell@yahoo.com>
2019-01-25 21:47:37 -0700

    Cards are now presented

--- MonoDragons.GGJ/GGJ/Scenes/GameScene.cs
+++ MonoDragons.GGJ/GGJ/Scenes/GameScene.cs
@@ -2,14 +2,18 @@
 using MonoDragons.Core;
 using MonoDragons.Core.Scenes;
 using MonoDragons.Core.UserInterface;
+using MonoDragons.GGJ.Gameplay;
+using System.Collections.Generic;
 
 namespace MonoDragons.GGJ.Scenes
 {
     public class GameScene : ClickUiScene
     {
+
         public override void Init()
         {
             Add(new Label { Text = "You are in game!", Transform = new Transform2(new Vector2(0, 0), new Size2(1600, 800)) });
+            Add(new Hand(new Card(), new Card(), new Card()));
         }
     }
 }
