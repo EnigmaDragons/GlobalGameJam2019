b1a51134a6a749b3eb05fa73c47cc67ed9b89443
noah reinagel <noah.r9@gmail.com>
2019-01-26 17:34:33 -0700

    moved some stuff to character state

--- MonoDragons.GGJ/GGJ/Gameplay/Character.cs
+++ MonoDragons.GGJ/GGJ/Gameplay/Character.cs
@@ -6,10 +6,6 @@ namespace MonoDragons.GGJ.Gameplay
 {
     public class Character
     {
-        private int _incomingDamage = 0;
-        private int _availableBlock = 0;
-        private List<object> _onNotDamaged = new List<object>();
-        private List<int> _damageTakenMultipliers = new List<int>();
         private readonly Player _player;
         private readonly GameData _data;
         private CharacterState State => _data[_player];
@@ -31,26 +27,26 @@ namespace MonoDragons.GGJ.Gameplay
         private void OnAttacked(PlayerDamageProposed e)
         {
             if (e.Target == _player)
-                _incomingDamage += e.Amount;
+                State.IncomingDamage += e.Amount;
         }
 
         private void OnDefended(PlayerBlockProposed e)
         {
             if (e.Target == _player)
-                _availableBlock += e.Amount;
+                State.AvailableBlock += e.Amount;
         }
 
         private void OnNotDamagedEffectQueued(OnNotDamagedEffectQueued e)
         {
             if (e.Target == _player)
-                _onNotDamaged.Add(e.Event);
+                State.OnNotDamaged.Add(e.Event);
         }
 
         private void OnCardTypeSelected(CardSelected e)
         {
             if (OpponentState.NextAttackBonus > 0 && e.Player != _player && _data.AllCards[e.CardId].Type == CardType.Attack)
             {
-                _incomingDamage += OpponentState.NextAttackBonus;
+                State.IncomingDamage += OpponentState.NextAttackBonus;
                 OpponentState.NextAttackBonus = 0;
             }
         }
@@ -64,32 +60,32 @@ namespace MonoDragons.GGJ.Gameplay
         private void OnDamageTakenMultiplied(DamageTakenMultiplied e)
         {
             if (e.Target == _player)
-                _damageTakenMultipliers.Add(e.Multiplier);
+                State.DamageTakenMultipliers.Add(e.Multiplier);
         }
 
         private void Resolve()
         {
-            var incomingDamage = _incomingDamage;
-            _damageTakenMultipliers.ForEach(x => incomingDamage = x * incomingDamage);
-            if (incomingDamage > _availableBlock)
+            var incomingDamage = State.IncomingDamage;
+            State.DamageTakenMultipliers.ForEach(x => incomingDamage = x * incomingDamage);
+            if (incomingDamage > State.AvailableBlock)
             {
-                var damage = incomingDamage - _availableBlock;
+                var damage = incomingDamage - State.AvailableBlock;
                 State.HP -= damage;
                 Event.Publish(new PlayerDamaged { Amount = damage, Target = State.Player });
             }
             else
             {
-                _onNotDamaged.ForEach(Event.Publish);
+                State.OnNotDamaged.ForEach(Event.Publish);
             }
             Reset();
         }
 
         private void Reset()
         {
-            _incomingDamage = 0;
-            _availableBlock = 0;
-            _onNotDamaged = new List<object>();
-            _damageTakenMultipliers = new List<int>();
+            State.IncomingDamage = 0;
+            State.AvailableBlock = 0;
+            State.OnNotDamaged = new List<object>();
+            State.DamageTakenMultipliers = new List<int>();
         }
     }
 }
--- MonoDragons.GGJ/GGJ/Gameplay/CharacterState.cs
+++ MonoDragons.GGJ/GGJ/Gameplay/CharacterState.cs
@@ -1,18 +1,27 @@
 ﻿
+using System.Collections.Generic;
+
 namespace MonoDragons.GGJ.Gameplay
 {
     public class CharacterState
     {
         public Player Player { get; }
         public int HP { get; set; }
-        public int NextAttackBonus { get; set; }
         public PlayerCardsState Cards { get; set; }
+
+        public int NextAttackBonus { get; set; }
+        public int IncomingDamage { get; set; }
+        public int AvailableBlock { get; set; }
+        public List<object> OnNotDamaged { get; set; }
+        public List<int> DamageTakenMultipliers { get; set; }
 
         public CharacterState(Player player, int hp, PlayerCardsState cards)
         {
             Player = player;
             HP = hp;
             Cards = cards;
+            OnNotDamaged = new List<object>();
+            DamageTakenMultipliers = new List<int>();
         }
     }
 }
