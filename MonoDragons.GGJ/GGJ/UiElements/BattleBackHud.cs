a0c0e1dc5f2ad05abe4b283400f60d85404610b0
SilasReinagel <silas.reinagel@gmail.com>
2019-01-27 18:17:00 -0700

    Moved Char UI Elements to back. Move Battle Top to front. Closes #191

--- MonoDragons.GGJ/GGJ/UiElements/BattleBackHud.cs
+++ MonoDragons.GGJ/GGJ/UiElements/BattleBackHud.cs
@@ -1,4 +1,5 @@
-﻿using MonoDragons.Core;
+﻿using System;
+using MonoDragons.Core;
 using MonoDragons.Core.Engine;
 using MonoDragons.Core.UserInterface;
 using System.Collections.Generic;
@@ -6,21 +7,47 @@ using Microsoft.Xna.Framework;
 
 namespace MonoDragons.GGJ.UiElements
 {
-    class BattleBackHud : IVisual
+    class BattleBackHud : IVisualAutomaton
     {
+        private readonly GameData _data;
+        private static readonly Size2 HpSize = new Size2(68, 68);
+        private readonly Label _cowboyHp = new Label { Transform = new Transform2(UI.OfScreen(0.008f, 0.78f), HpSize) };
+        private readonly Label _houseHp = new Label { Transform = new Transform2(UI.OfScreen(0.946f, 0.78f), HpSize) };
+        private readonly Label _cowboyNrg = new Label { Transform = new Transform2(UI.OfScreen(0.008f, 0.88f), HpSize), TextColor = UiConsts.DarkBrown };
+        private readonly Label _houseNrg = new Label { Transform = new Transform2(UI.OfScreen(0.946f, 0.88f), HpSize), TextColor = UiConsts.DarkBrown };
         private readonly List<IVisual> _visuals;
 
-        public BattleBackHud(Player player)
+        public BattleBackHud(Player player, GameData data)
         {
+            _data = data;
             _visuals = new List<IVisual>
             {
-                new Sprite{ Image= $"House/floor-{player.ToString().ToLower()}", Transform = new Transform2(UI.OfScreenSize(1.0f, 1.0f))} 
+                new Sprite{ Image= $"House/floor-{player.ToString().ToLower()}", Transform = new Transform2(new Vector2(0, 26), UI.OfScreenSize(1.0f, 1.0f))} ,
+                new UiImage { Image= "UI/heart", Transform = new Transform2(UI.OfScreen(0.008f, 0.78f), HpSize)},
+                new UiImage { Image= "UI/heart", Transform = new Transform2(UI.OfScreen(0.946f, 0.78f), HpSize)},
+                _cowboyHp,
+                _houseHp,
+                new UiImage { Image= "UI/energy", Transform = new Transform2(UI.OfScreen(0.008f, 0.88f), HpSize)},
+                new UiImage { Image= "UI/energy", Transform = new Transform2(UI.OfScreen(0.946f, 0.88f), HpSize)},
+                _cowboyNrg,
+                _houseNrg,
             };
         }
 
         public void Draw(Transform2 parentTransform)
         {
-            _visuals.ForEach(x => x.Draw(new Transform2(new Vector2(0, 26))));
+            _visuals.ForEach(x => x.Draw(parentTransform));
+        }
+        
+        public void Update(TimeSpan delta)
+        {
+            if (!_data.IsInitialized)
+                return;
+            
+            _cowboyHp.Text = _data.CowboyState.HP.ToString();
+            _houseHp.Text = _data.HouseState.HP.ToString();
+            _cowboyNrg.Text = _data.CowboyState.Energy.ToString();
+            _houseNrg.Text = _data.HouseState.Energy.ToString();
         }
     }
 }
